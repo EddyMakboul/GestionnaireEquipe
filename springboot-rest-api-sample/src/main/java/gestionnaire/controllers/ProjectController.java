0042eafaf3404bd6d521f702f7c182602b49619f
FloFloDht <flo.11.italia@gmail.com>
2021-10-18 21:59:14 +0200

    Create ProjectController.java

---
+++ springboot-rest-api-sample/src/main/java/gestionnaire/controllers/ProjectController.java
@@ -0,0 +1,13 @@
+package gestionnaire.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/project")
+public class ProjectController {
+
+    
+
+}
