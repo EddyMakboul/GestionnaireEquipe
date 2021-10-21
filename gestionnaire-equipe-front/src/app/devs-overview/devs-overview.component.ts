4c02a3e8fe8b4e431bf1c26c33b0a7d0de72224c
eddy makboul <eddymakboul@icloud.com>
2021-10-21 15:57:29 +0200

    EML - traitement donnée pour la liste de dev

--- gestionnaire-equipe-front/src/app/devs-overview/devs-overview.component.ts
+++ gestionnaire-equipe-front/src/app/devs-overview/devs-overview.component.ts
@@ -1,4 +1,7 @@
+import { error } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../shared/model/employee.model';
+import { EmployeeService } from '../shared/services/employee.service';
 
 @Component({
   selector: 'app-devs-overview',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DevsOverviewComponent implements OnInit {
 
-  constructor() { }
+  employes: Employee[];
+
+  constructor(private employeService: EmployeeService) { }
 
   ngOnInit(): void {
+    this.employeService.findAll().subscribe(
+      employes => {
+        this.employes = employes;
+      }
+    )
+  }
+
+  delete(id_employe: number): void {
+    this.employeService.delete(id_employe).subscribe(
+      response => {
+        console.log(response);
+      },
+      errors => {
+        console.log(errors)
+      }
+    )
   }
 
 }
