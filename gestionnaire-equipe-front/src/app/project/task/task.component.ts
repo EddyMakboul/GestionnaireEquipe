28d5a9680354d757c092d9cb9b83380bedb5cfd5
eddy makboul <eddymakboul@icloud.com>
2021-10-22 10:38:56 +0200

    EML - parsing list tache

--- gestionnaire-equipe-front/src/app/project/task/task.component.ts
+++ gestionnaire-equipe-front/src/app/project/task/task.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Projet } from 'src/app/shared/model/projet.model';
+import { Tache } from 'src/app/shared/model/tache.model';
+import { TacheService } from 'src/app/shared/services/tache.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-task',
@@ -10,9 +13,21 @@ export class TaskComponent implements OnInit {
 
   @Input() projet: Projet;
 
-  constructor() { }
+  freeList: Tache[];
+  inProgressList: Tache[];
+  doneList: Tache[];
+  bigList: Tache[];
+
+  constructor(private tacheService: TacheService) { }
 
   ngOnInit(): void {
+    this.tacheService.getAllTacheByProjetId(this.projet.id).subscribe(
+      data => {
+        this.freeList = data.filter(tache => tache.employee == null && !tache.finished)
+        this.inProgressList = data.filter(tache => tache.employee && !tache.finished)
+        this.doneList = data.filter(tache => tache.finished)
+      }
+    );
   }
 
 }
