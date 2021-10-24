e5932190149b7a3a362519b7cb7ddb9806b1aa19
FloFloDht <flo.11.italia@gmail.com>
2021-10-24 17:49:33 +0200

    Doc pour TacheController

--- springboot-rest-api-sample/src/main/java/gestionnaire/controllers/TacheController.java
+++ springboot-rest-api-sample/src/main/java/gestionnaire/controllers/TacheController.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Controller pour les Taches, permet de gérer les actions sur les taches
+ */
 @RestController
 @RequestMapping(value = "/api/taches")
 public class TacheController {
@@ -31,23 +34,41 @@ public class TacheController {
     @Autowired
     ProjetRepository projetRepository;
 
+    /**
+     * Methode GET, permet de récupérer toutes les taches actives
+     * @return
+     */
     @GetMapping()
     public Iterable<Tache> getAllActiveTasks(){
         return tacheRepository.findAll();
     }
 
+    /**
+     * Methode Post, permet de créer une nouvelle tache
+     * @param tache une Tache
+     * @return
+     */
     @PostMapping()
     public Tache createTask(@RequestBody Tache tache){
         tacheRepository.save(tache);
         return tache;
     }
 
+    /**
+     * Methode DELETE, permet de supprimet une tache via son id
+     * @param id l'id d'une Tache
+     */
     @DeleteMapping()
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteTask(@PathVariable long id){
         tacheService.deleteTacheById(id);
     }
 
+    /**
+     * Methode PUT, permet de mettre à jour une tache
+     * @param task une Tache
+     * @return
+     */
     @PutMapping()
     public ResponseEntity<Tache> updateTask (@RequestBody Tache task){
         Tache updateTask;
@@ -66,6 +87,11 @@ public class TacheController {
         return new ResponseEntity<>(updateTask, HttpStatus.CREATED);
     }
 
+    /**
+     * Methode GET, permet de récupérer une tache d'un projet
+     * @param id l'id d'un Projet
+     * @return
+     */
     @GetMapping("/projet/{id}")
     public ResponseEntity<List<Tache>> getTastFromProject(@PathVariable Long id){
         Optional<Projet> projet = projetRepository.findById(id);
@@ -76,6 +102,11 @@ public class TacheController {
         return new ResponseEntity<>(tacheRepository.findTacheByProjet(projet.get()),HttpStatus.OK);
     }
 
+    /**
+     * Methode GET, permet de récupérer une tache via son id
+     * @param id l'id d'une Tache
+     * @return
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Tache> findTacheById (@PathVariable Long id)
     {
@@ -88,6 +119,12 @@ public class TacheController {
         return new ResponseEntity<>(tache.get(), HttpStatus.OK);
     }
 
+    /**
+     * Methode PUT, permet de mettre à jour une Tache
+     * @param id l'id d'une Tache
+     * @param finished un booleen
+     * @return
+     */
     @PutMapping("/{id}")
     public ResponseEntity<Tache> updateStatusTache	 (@PathVariable Long id,@RequestBody Boolean finished)
     {
